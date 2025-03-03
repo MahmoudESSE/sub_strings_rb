6d921fdd632ed4b842ee3227abe324f60d4eedef
MahmoudESSE <mahmoudessehayli@gmail.com>
2025-03-03 22:15:55 +0000

    feat: find str occurances in dictionary
    Signed-off-by: MahmoudESSE <mahmoudessehayli@gmail.com>

--- main.rb
+++ main.rb
@@ -1,3 +1,16 @@
 # frozen_string_literal: true
 
-puts "Hello, World!"
+# @param str [String]
+# @param dictionary [Array<String>]
+def substrings(str, dictionary)
+  # @param word [String]
+  # @param occurance [Integer]
+  dictionary.each_with_object(Hash.new(0)) do |word, occurance|
+    occurance[word.to_sym] += 1 if word.eql? str
+    occurance
+  end
+end
+
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+
+pp substrings('below', dictionary)
