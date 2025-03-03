67dce5a390631b7756961ae42241609c44e4caf6
MahmoudESSE <mahmoudessehayli@gmail.com>
2025-03-03 22:22:37 +0000

    feat: handle sentences
    Signed-off-by: MahmoudESSE <mahmoudessehayli@gmail.com>

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ def substrings(str, dictionary)
   # @param word [String]
   # @param occurance [Integer]
   dictionary.each_with_object(Hash.new(0)) do |word, occurance|
-    occurance[word.to_sym] += 1 if word.eql? str
+    occurance[word.to_sym] += 1 if word.downcase.eql? str.downcase[word]
     occurance
   end
 end
@@ -14,3 +14,5 @@ end
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
 pp substrings('below', dictionary)
+
+pp substrings("Howdy partner, sit down! How's it going?", dictionary)
